06e98a680cd134c34aece63d14b6eed4664e8fbb
Murali Raju <murali.raju@appliv.com>
2014-03-28 14:52:07 -0400

    scaffold

---
+++ ferry/config/spark/sparkconfig.py
@@ -0,0 +1,38 @@
+# Copyright 2014 OpenCore LLC
+# Copyright 2014, Murali Raju, Velankani Information Systems,Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import sys
+import time
+import sh
+from string import Template
+from ferry.install import FERRY_HOME
+from ferry.docker.fabric import DockerFabric
+
+
+class SparkInitializer(object):
+    """
+    Create a new initializer
+    Param user The user login for the git repo
+    """
+    def __init__(self):
+        self.template_dir = None
+        self.template_repo = None
+
+        self.container_data_dir = SparkConfig.data_directory
+        self.container_log_dir = SparkConfig.log_directory
+
+
